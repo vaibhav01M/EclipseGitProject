855b9e0c5cb0c12a18f03f841d93494595a8a215
Vaibhav01M <vaibhavvmhaskar@gmail.com>
2019-03-23 15:27:04 +0530

    Second Commit and push for Branch1-VM

--- GitDemo/src/Test.java
+++ GitDemo/src/Test.java
@@ -7,7 +7,7 @@ public abstract class Test {
 		System.out.println("Hello ,  Vaibhav ");
 		System.out.println("My first commit through eclipse");
 		System.out.println("Branch name is : Branch1-VM");
-		System.out.println("new feature branch");
+		System.out.println("new  feature branch");
 	}
 
 }
